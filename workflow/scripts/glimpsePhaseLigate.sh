f9e5461f191e08007072e0ebc72420c3fcacf5f4
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2021-07-23 09:18:04 +0100

    add glimpse shell script

---
+++ workflow/scripts/glimpsePhaseLigate.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+CHROM=$1
+VCF=$2
+REF=$3
+MAP=$4
+CHUNK=$5
+THREADS=$6
+
+while IFS="" read -r LINE || [ -n "$LINE" ];
+do
+printf -v ID "%02d" $(echo $LINE | cut -d" " -f1)
+IRG=$(echo $LINE | cut -d" " -f3)
+ORG=$(echo $LINE | cut -d" " -f4)
+OUT="results/vcfs/imputed.${CHROM}.${ID}.vcf"
+
+workflow/scripts/GLIMPSE/static_bins/GLIMPSE_phase_static --input ${VCF} --reference ${REF} --map ${MAP} --input-region ${IRG} --output-region ${ORG} --output ${OUT} --thread ${THREADS}
+
+bcftools index ${OUT} 
+done < $CHUNK
+
+find results/vcfs/ -name "imputed.${CHROM}*.vcf.gz" | sort > results/vcfs/${CHROM}.vcf.list
+
+echo "Imputation complete..."
+
+LST=results/vcfs/${CHROM}.vcf.list
+OUT=results/vcfs/imputed.${CHROM}.vcf.gz
+
+workflow/scripts/GLIMPSE/ligate/bin/GLIMPSE_ligate --input ${LST} --output ${OUT} --thread ${THREADS}
+
+echo "Ligation complete..."
